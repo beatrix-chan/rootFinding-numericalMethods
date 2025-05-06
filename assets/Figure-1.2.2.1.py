7b31cf880b7525f86d2224aea15109cc9f66f3ce
Beatrix CHAN <beatrix.chan@durham.ac.uk>
2025-05-06 10:50:39 +0100

    Add: Figure-1.2.2.1

---
+++ assets/Figure-1.2.2.1.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f(x):
+    return 2 * (x**2) + 7 * x - 4
+
+def df(x):
+    return 4 * x + 7
+
+x0 = 1
+
+m = df(x0)
+b = f(x0) - m * x0
+
+x = np.linspace(-6, 6, 400)
+y = f(x)
+tangent = m * x + b
+
+plt.plot(x, y, c='#cac486', label='$f(x) = 2x^2 + 7x - 4$', zorder=1)
+plt.axvline(0, c='#807b81', zorder=0)
+plt.axhline(0, c='#807b81', zorder=0)
+plt.plot(x, tangent, c='#72969d', label='Tangent line at $x = {x0}$', zorder=2)
+plt.scatter(x0, f(x0), marker='*', c='#4b6e74', s=50, label='initial guess', zorder=3)
+
+plt.xlim(-0.8, 2.3)
+plt.ylim(-5, 20)
+plt.grid(True, c='grey', alpha=0.5)
+
+plt.xlabel('$x$')
+plt.ylabel('$f(x)$')
+plt.legend()
+
+plt.show()
