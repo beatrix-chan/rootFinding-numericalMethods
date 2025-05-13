2b5c3e34812d0a8dcf9d4de688047f4582495ac1
Beatrix CHAN <beatrix.chan@durham.ac.uk>
2025-05-13 12:07:28 +0000

    Update: Figure-1.2.3.1.py
    Remove extra `$` that caused incorrect legend output. (`.png` file to be updated)

--- assets/Figure-1.2.3.1.py
+++ assets/Figure-1.2.3.1.py
@@ -12,7 +12,7 @@ y = f(x)
 plt.plot(x, y, c='#cac486', label='$f(x) = 2x^2 + 7x - 4$', zorder=1)
 plt.axhline(0, c='#807b81', zorder=0)
 plt.axvline(0, c='#807b81', zorder=0)
-plt.plot([x0, x1], [f(x0), f(x1)], c='#72969d', label=f'Secant line between $x = {x0}$ and $x = ${x1}$', zorder=2)
+plt.plot([x0, x1], [f(x0), f(x1)], c='#72969d', label=f'Secant line between $x = {x0}$ and $x = {x1}$', zorder=2)
 plt.scatter([x0, x1], [f(x0), f(x1)], marker='*', c='#4b6e74', s=50, label='initial guesses', zorder=3)
 
 plt.xlim(0, 2)
